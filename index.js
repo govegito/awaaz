dd91d2e11273086aec17c8c52bf22b682c1caef9
2017uec1581@mnit.ac.in <2017uec1581@mnit.ac.in>
2020-01-18 22:18:54 +0530

    added hello

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var app     = express();
 var mongoose		=require('mongoose');
 //mongoose.connect("mongodb://localhost:27017/awaaz", { useNewUrlParser: true });
 
-
+app.get("/", function(req, res){
+	res.send('Hello motherfucker');
+});
 
 app.listen(3000, function(){
 	console.log("okay");
