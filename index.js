dac7abc05c177dbaef869aa1538ea0039479d533
govegito <jayeshsuthar826@gmail.com>
2020-01-18 21:32:24 +0530

    intial build

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app     = express();
+var mongoose		=require('mongoose');
+//mongoose.connect("mongodb://localhost:27017/awaaz", { useNewUrlParser: true });
+
+
+
+app.listen(3000, function(){
+	console.log("okay");
+});
