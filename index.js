72f75036107a3fb6aa84401482c17ee0e3373537
govegito <jayeshsuthar826@gmail.com>
2020-01-18 23:02:44 +0530

    added for dev branch

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mongoose		=require('mongoose');
 //mongoose.connect("mongodb://localhost:27017/awaaz", { useNewUrlParser: true });
 
 app.get("/", function(req, res){
-	res.send('abbey falle');
+	res.send('hello from dev branch');
 });
 
 app.listen(3000, function(){
