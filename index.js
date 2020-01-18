3040b0e9918fc3d04c9f62c2b042eab1d79e0746
govegito <jayeshsuthar826@gmail.com>
2020-01-18 22:30:33 +0530

    added new message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mongoose		=require('mongoose');
 //mongoose.connect("mongodb://localhost:27017/awaaz", { useNewUrlParser: true });
 
 app.get("/", function(req, res){
-	res.send('Hello motherfucker');
+	res.send('abbey falle');
 });
 
 app.listen(3000, function(){
