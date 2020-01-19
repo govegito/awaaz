bb7d22ebc437e63236b0fed04d694b5fc4446403
govegito <jayeshsuthar826@gmail.com>
2020-01-19 14:15:57 +0530

    added nothing

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ var app     = express();
 var mongoose		=require('mongoose');
 bodyparser=     require("body-parser");
 //mongoose.connect("mongodb://localhost:27017/awaaz", { useNewUrlParser: true });
+app.set('view engine', 'ejs');
 app.use('/views/assets', express.static('views/assets'));
 app.use(bodyparser.urlencoded({extended:true}));
 
 
 app.get("/", function(req, res){
-	res.send('hello from anurags dev branch');
+	res.render('landing.ejs');
 });
 
 app.listen(3000, function(){
