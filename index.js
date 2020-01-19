003b5255bd8e558a3f50d37de2a3ad9907a3c43d
govegito <jayeshsuthar826@gmail.com>
2020-01-19 12:01:06 +0530

    added folders

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app     = express();
 var mongoose		=require('mongoose');
+bodyparser=     require("body-parser");
 //mongoose.connect("mongodb://localhost:27017/awaaz", { useNewUrlParser: true });
+app.use('/views/assets', express.static('views/assets'));
+app.use(bodyparser.urlencoded({extended:true}));
+
 
 app.get("/", function(req, res){
 	res.send('hello from dev branch');
