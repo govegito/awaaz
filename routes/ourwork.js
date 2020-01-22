b068f0f5603f9a35b9bdaf317fc744c311291c4f
govegito <jayeshsuthar826@gmail.com>
2020-01-23 01:07:34 +0530

    added our work

--- routes/ourwork.js
+++ routes/ourwork.js
@@ -2,8 +2,13 @@ var express=require("express");
 var router=express.Router();
 
 
-router.get("/", function(req,res){
-    res.render("../views/ourwork/index.ejs");
+router.get("/ourapp", function(req,res){
+    res.render("../views/ourwork/ourapp.ejs");
 });
+
+router.get("/training", function(req,res){
+    res.render("../views/ourwork/training.ejs");
+});
+
 
 module.exports=router;
